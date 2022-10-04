d7d8acdac8a8b20aa3a488fc1b60a434f18c061d
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2022-10-04 15:33:52 +0530

    update app.py

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ app=Flask(__name__)
 @app.route("/")
 def fun1():
     return render_template("info.html")
- 
+ https://docs.google.com/spreadsheets/d/1H-rkyHggDJJI-QNh5SgG9u4zzQCEiow79mCMY_8I9CI/edit?usp=sharing
 @app.route("/show",methods = ["post"])
 def home():
     nm = request.form['name']
     reg_id = int(request.form['reg_id'])
-    sheet_id1='11LNkA1BML-sOJF65XBHSYMV9-s8AVeJhoy-rhgOUfLY'
-    sheet_id2 = '1aJFuKyLaZRdWZ-S81T1CylFOOsvZDbVRlvg-pVHTO50'
-    df = pd.concat(map(pd.read_csv,[f"https://docs.google.com/spreadsheets/d/{sheet_id1}/export?format=csv", f"https://docs.google.com/spreadsheets/d/{sheet_id2}/export?format=csv"]))
+    sheet_id1='1H-rkyHggDJJI-QNh5SgG9u4zzQCEiow79mCMY_8I9CI'
+    #sheet_id2 = '1aJFuKyLaZRdWZ-S81T1CylFOOsvZDbVRlvg-pVHTO50'
+    #df = pd.concat(map(pd.read_csv,[f"https://docs.google.com/spreadsheets/d/{sheet_id1}/export?format=csv", f"https://docs.google.com/spreadsheets/d/{sheet_id2}/export?format=csv"]))
+    df = pd.concat(map(pd.read_csv,[f"https://docs.google.com/spreadsheets/d/{sheet_id1}/export?format=csv"]))
+   
     result1 = df[df['Registration id'] ==reg_id ]
     return result1.to_html()
     
