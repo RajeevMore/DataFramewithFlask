cb8d941a47859ba4c05030353d780ceabe8e65ce
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2022-10-15 15:27:16 +0530

    update app.py

--- app.py
+++ app.py
@@ -11,9 +11,28 @@ def home():
     #sheet_id1='1H-rkyHggDJJI-QNh5SgG9u4zzQCEiow79mCMY_8I9CI'
    
     #df = pd.concat(map(pd.read_csv,[f"https://docs.google.com/spreadsheets/d/{sheet_id1}/export?format=csv"]))
-    df = pd.read_csv("mockdata.csv")
-    result1 = df[df['Registration id'] == reg_id ]
-    return result1.to_html()
+    #df = pd.read_csv("mockdata.csv")
+    #result1 = df[df['Registration id'] == reg_id ]
+    #return result1.to_html()
+    
+     df = pd.read_csv("mockdata.csv")
+    
+    def do_lower(name):
+        return name.lower()
+
+    df['Student Name']=df["Student Name"].apply(do_lower)
+    result1 = df[df['Registration id'] ==reg_id]
+    name = nm.lower()
+    result2 = result1[result1["Student Name"].str.contains(name)]
+      
+    return '<h1> Welcome {}, Your Mock Interview Scores are as follows</h1>'.format(name)+"<br/>"+result2.to_html()+"<br/><br/> If you have provided correct name then infomation will display otherwise not" 
+    
+
+
+
+
+
+
     
 if __name__ == "__main__":
     app.run(debug=True)
