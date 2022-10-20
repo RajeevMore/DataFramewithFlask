a04dc3adfe60b86f080f83c8cc4bc10116f337d5
Rajeev More <100182072+RajeevMore@users.noreply.github.com>
2022-10-20 15:27:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
     
 
     df = pd.read_csv("mockdata.csv")
-    df.drop(columns="Expert Name")
+    df = df.drop(columns="Expert Name")
     result1 = df[df['Registration id'] ==reg_id]    
     return '<h1> Welcome, Your Mock Interview Scores are as follows</h1><br/>'+result1.to_html(index=False) 
     
